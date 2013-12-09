8b92e50e9e18da2aad5ec1de8a18b642e301a7ac
Vitor Baptista <vitor@vitorbaptista.com>
2013-12-09 14:50:44 -0300

    Otimizações à geração do gdf
    Agora nós pré-calculamos o peso dos nós, assim o arquivo gerado é *MUITO* menor
    e o Gephi não usa tanta memória.

--- graph.rb
+++ graph.rb
@@ -1,6 +1,9 @@
+require "date"
 require "./db"
 
-FILEPATH = "edges.gdf"
+YEAR = "2004"
+UNIXTIME = Date.parse("#{YEAR}-01-01").to_time.to_i
+FILEPATH = "edges-#{YEAR}.gdf"
 
 
 def generate_nodes
@@ -19,29 +22,54 @@ end
 
 def generate_edges
   db = DB.open
-  votacoes = db.execute("SELECT * FROM votacoes ORDER BY id DESC LIMIT 40")
 
   f = File.open(FILEPATH, 'a')
   # f.write("Weight;Type;Source;Target;Voto\n")
-  f.write("edgedef>node1 VARCHAR, node2 VARCHAR, voto VARCHAR\n")
+  f.write("edgedef>node1 VARCHAR, node2 VARCHAR, weight DOUBLE, time INTEGER\n")
 
-  for votacao in votacoes
-    votos = db.execute("SELECT * FROM votos WHERE votacao_id = ?", votacao[0])
+  votos = db.execute <<-eos
+    SELECT * FROM votos AND votacao_id IN
+      (SELECT id FROM votacoes WHERE
+       data > date("#{YEAR}-01-01") AND data < date("#{YEAR}-12-31"));
+  eos
+  # Cria tuplas ["Deputado", "Voto"] e
+  tuplas = votos.map{|v| [v[2], v[5]]}
 
-    # Cria tuplas ["Deputado", "Voto"] e 
-    tuplas = votos.map{|v| [v[2], v[5]]}
-    
-    # Faz combinação
-    pares_votos = tuplas.combination(2)
+  # Faz combinação
+  pares_votos = tuplas.combination(2)
 
-    # Itera combinação e se votou igual, escreve no grafo
-    for par_voto in pares_votos
-      if par_voto[0][1] == par_voto[1][1]
-        f.write("\"#{par_voto[0][0]}\",\"#{par_voto[1][0]}\",#{par_voto[0][1]}\n")
-      end
+  deputados = {}
+  # Itera combinação e se votou igual, escreve no grafo
+  for par_voto in pares_votos
+    voto0 = par_voto[0][1]
+    voto1 = par_voto[1][1]
+    next unless ["Sim", "Não"].include?(voto0) and ["Sim", "Não"].include?(voto1)
+    deputados[par_voto[0][0]] ||= {}
+    deputados[par_voto[0][0]][par_voto[1][0]] ||= 0
+    if par_voto[0][1] == par_voto[1][1] and
+      deputados[par_voto[0][0]][par_voto[1][0]] += 1
+      # f.write("\"#{par_voto[0][0]}\",\"#{par_voto[1][0]}\",#{par_voto[0][1]}\n")
+    else
+      deputados[par_voto[0][0]][par_voto[1][0]] -= 1
+    end
+  end
+
+  min_value = 99999999999999999999
+  max_value = -99999999999
+  count = 0
+  deputados.each_pair do |source, values|
+    values.each_pair do |target, value|
+      min_value = value if value < min_value
+      max_value = value if value > max_value
+      f.write("\"#{source}\",\"#{target}\",#{value},#{UNIXTIME}\n") if value > 0
+      count += 1 if value <= 0
     end
   end
     
+  STDERR.puts deputados.length
+  STDERR.puts min_value
+  STDERR.puts max_value
+  STDERR.puts count
   f.close
 end
 
