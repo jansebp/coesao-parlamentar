0d599b87288fe2f9d005e8ac72e23c4e0266006c
Fernando Brito <email@fernandobrito.com>
2013-12-12 22:55:34 -0300

    Remove votações onde mais de 95% dos votos foram sim ou não
    Também mostra na view o total antes e depois do filtro
    
    O código do filtro ficou muito feio pois a variável
    onde o filtro é feito ('votacoes') foi criada a partir
    de 'votos', e 'votos' é usada depois para outras coisas.
    Foi necessário filtrar os votos depois de filtrar as
    votações e mover a criação de 'votacoes_deputados'
    para depois destes filtros.

--- data/adjacency-matrix.rb
+++ data/adjacency-matrix.rb
@@ -6,7 +6,21 @@ require "./db"
 YEARS = (2000..2008)
 PARTIES = %w(PT PMDB PSOL)
 
+FILTER_THRESHOLD = 0.95
+
 FILEPATH = "../app/data/PARTY-YEAR.json"
+
+def votacao_filter(db, id)
+  votos = db.execute <<-eos
+    SELECT * FROM votos WHERE votacao_id = #{id}
+  eos
+
+  sim = votos.select { |v| v[5] == "Sim" }.size
+  nao = votos.select { |v| v[5] == "Não" }.size
+
+  total = votos.size
+  (sim.to_f / total) <= FILTER_THRESHOLD && (nao.to_f / total) <= FILTER_THRESHOLD
+end
 
 def generate_json(party, year)
   db = DB.open
@@ -23,14 +37,21 @@ def generate_json(party, year)
   eos
 
   if votos.empty?
-    puts "[Matrix] Empty matrix for #{party} in #{year}. No file created."
+    puts "[Matrix] Empty matrix for #{party} in #{year}. No votes in database."
     return
   else
     puts "[Matrix] Generating matrix for #{party} in #{year}"
   end
 
-  votacoes_deputados = votos.group_by { |voto| voto[2] } # nome
   votacoes = votos.group_by { |voto| voto[1] } # votacao_id
+  total_voting_sessions = votacoes.size
+  votacoes.keep_if { |id| votacao_filter(db, id) }
+
+  # Votacoes has been filtered, but votos has not
+  # this will also filter votos
+  votos.keep_if { |v| votacoes.select { |k| k == v[1] }.size > 0 } 
+
+  votacoes_deputados = votos.group_by { |voto| voto[2] } # nome
   votacoes.each_pair { |key, votacao| votacoes[key] = votacao.group_by { |v| v[5] } }
 
   # deputados_comunidades = {}
@@ -47,6 +68,7 @@ def generate_json(party, year)
     votos.each do |voto|
       votacao_id = voto[1]
       votacao = votacoes[votacao_id]
+
       escolha = voto[5]
       links[index] ||= {}
       if escolha == "Sim"
@@ -82,10 +104,10 @@ def generate_json(party, year)
     end
   end
 
-  stats = { 'voting_sessions' => votacoes.size }
+  stats = { 'filtered_voting_sessions' => votacoes.size, 'total_voting_sessions' => total_voting_sessions }
 
   json = { 'links' => formatted_links, 'nodes' => deputados, 'stats' => stats }
-  
+
   filepath = FILEPATH.gsub("PARTY", party.downcase).gsub("YEAR", year.to_s)
 
   File.open(filepath, 'w+') do |file|
